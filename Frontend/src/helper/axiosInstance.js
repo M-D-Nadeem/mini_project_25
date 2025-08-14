a6d57f2b6a8d29ff8fa32875cfe3f2b312ef4fe6
M-D-Nadeem <149703432+M-D-Nadeem@users.noreply.github.com>
2025-08-14 15:34:17 +0530

    All change

--- Frontend/src/helper/axiosInstance.js
+++ Frontend/src/helper/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance=axios.create()
 
-// axiosInstance.defaults.baseURL="https://mini-project-25-zomg.onrender.com/app"
-axiosInstance.defaults.baseURL="http://localhost:9000/app"
+axiosInstance.defaults.baseURL="https://mini-project-25-zomg.onrender.com/app"
+// axiosInstance.defaults.baseURL="http://localhost:9000/app"
 axiosInstance.defaults.withCredentials=true
 
 export default axiosInstance
